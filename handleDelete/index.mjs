4b1213799c99ba9e5e7685e94b5600b79ac2aa89
ReeceRenninger <renningerreece@gmail.com>
2023-06-08 16:11:45 -0700

    all CRUD working

---
+++ handleDelete/index.mjs
@@ -0,0 +1,28 @@
+import dynamoose from 'dynamoose';
+
+const schema = new dynamoose.Schema({
+  'id': String,
+  'name': String,
+});
+
+const personModel = dynamoose.model('peoples', schema); // 'peoples' is the model taking the db name and schema above
+
+export const handler = async (event) => {
+  let id = event.pathParameters.id;
+
+  const response = { statusCode: null, body: null, };
+
+  try {
+    await personModel.delete(id);
+    response.body = `${id} was deleted!`;
+    response.statusCode = 200;
+  } catch (error) {
+    response.body = JSON.stringify(error.message);
+    response.statusCode = 500;
+  }
+
+
+
+
+  return response;
+};
