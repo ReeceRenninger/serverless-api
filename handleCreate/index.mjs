8fd2e90e6aa6d615c77bf24132c640cd2c7fcb03
ReeceRenninger <renningerreece@gmail.com>
2023-06-08 16:01:17 -0700

    PUT working

--- handleCreate/index.mjs
+++ handleCreate/index.mjs
@@ -8,13 +8,15 @@ const schema = new dynamoose.Schema({
 const personModel = dynamoose.model('peoples', schema); // 'peoples' is the model taking the db name and schema above
 
 export const handler = async (event) => {
-  let parsedBody = JSON.parse(event.body);
+  let id = event.pathParameters.id;
+  let parsedData = JSON.parse(event.body);
+  let updatedName = parsedData.name;
 
-  const response = { statusCode: null, body: null, };
+  const response = { statusCode: null, body: null };
 
   try {
-    let newPerson = await personModel.create(parsedBody);
-    response.body = JSON.stringify(newPerson);
+    let updatedPerson = await personModel.update({ 'id': id, 'name': updatedName });
+    response.body = JSON.stringify(updatedPerson);
     response.statusCode = 200;
   } catch (error) {
     response.body = JSON.stringify(error.message);
